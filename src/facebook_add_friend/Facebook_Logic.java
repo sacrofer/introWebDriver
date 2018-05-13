723a8423e06c6978a177a39121e3e7f8e6b3e9e7
sacrofer <38994980+sacrofer@users.noreply.github.com>
2018-05-13 00:47:24 -0500

    First commit for facebook add friend

---
+++ src/facebook_add_friend/Facebook_Logic.java
@@ -0,0 +1,10 @@
+package facebook_add_friend;
+
+import Generics.Automation;
+
+public class Facebook_Logic extends Automation {
+	
+	public static void main(String[] args) {
+		StartBrowser("Chrome", "http://facebook.com");
+	}	
+}
