9a6a31ee0b0070f937dab08088cdb1b8c0f37e5e
sacrofer <38994980+sacrofer@users.noreply.github.com>
2018-05-13 21:49:08 -0500

    Uses constants instead of strings and calls validation methods

--- src/Imdb/Imdb_main.java
+++ src/Imdb/Imdb_main.java
@@ -2,8 +2,15 @@ package Imdb;
 
 public class Imdb_main extends Imdb_Methods {
 
+	private static final String MOVIE_NAME = "It";
+	private static final String MOVIE_YEAR = "1990";
+	private static final String URL = "http://imdb.com";
+	private static final String BROWSER = "Chrome";
+
 	public static void main(String[] args) {
-		StartBrowser("Chrome", "http://imdb.com");
-		SearchMovie("It");
+		StartBrowser(BROWSER, URL);
+		SearchMovie(MOVIE_NAME, MOVIE_YEAR);
+		VerifyMovie (MOVIE_NAME, MOVIE_YEAR);
+		driver.quit();
 	}
 }
