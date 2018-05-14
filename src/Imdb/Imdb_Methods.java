954dd74bcd12df5c7de1d53d7fcc081045514761
sacrofer <38994980+sacrofer@users.noreply.github.com>
2018-05-13 21:46:46 -0500

    Locates correct movie and validates result

--- src/Imdb/Imdb_Methods.java
+++ src/Imdb/Imdb_Methods.java
@@ -1,5 +1,7 @@
 package Imdb;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -7,9 +9,35 @@ import Generics.Automation;
 
 public class Imdb_Methods extends Automation {
 	
-	protected static void SearchMovie(String movie) {
+	protected static void SearchMovie(String movie, String year) {
 		WebElement searchBox = driver.findElement(By.name("q"));
 		searchBox.clear();
 		searchBox.sendKeys(movie);
+		
+		WebElement searchBtn = driver.findElement(By.id("navbar-submit-button"));
+		searchBtn.click();
+		
+		LocateCorrectMovie(movie, year);
+	}
+	
+	protected static WebElement LocateCorrectMovie(String movie, String year) {
+		List<WebElement> Containers = driver.findElements(By.className("result_text"));
+		
+		for (WebElement container : Containers) {
+			System.out.println(container.getText());
+			if (container.getText().contains(movie) && container.getText().contains(year)) {
+				container.findElement(By.linkText(movie)).click();
+				return container;
+			}
+		}
+		return null;
+	}
+	
+	protected static void VerifyMovie (String movie, String year) {
+		if (driver.getTitle().contains(movie) && driver.getTitle().contains(year)) {			
+			System.out.println("Test Passed");
+		}else {
+			System.out.println("Test FAILED");
+		}
 	}
 }
