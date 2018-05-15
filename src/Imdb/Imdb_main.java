4ec1e0ff779127feb8e1e431f14d8f4d2addbb28
sacrofer <38994980+sacrofer@users.noreply.github.com>
2018-05-14 23:12:58 -0500

    Adds support for finding a movie also considering the director

--- src/Imdb/Imdb_main.java
+++ src/Imdb/Imdb_main.java
@@ -2,16 +2,16 @@ package Imdb;
 
 public class Imdb_main extends Imdb_Methods {
 
-	private static final String MOVIE_NAME = "It";
-	private static final String MOVIE_YEAR = "1990";
 	private static final String URL = "http://imdb.com";
 	private static final String BROWSER = "Chrome";
-	private static final String MOVIE_DIRECTOR = "RonClements";
+	private static final String MOVIE_NAME = "It";
+	private static final String MOVIE_YEAR = "1990";	
+	private static final String MOVIE_DIRECTOR = "Tommy Lee Wallace";
 
 	public static void main(String[] args) {
 		StartBrowser(BROWSER, URL);
 		SearchMovie(MOVIE_NAME, MOVIE_YEAR);
 		VerifyMovie (MOVIE_NAME, MOVIE_YEAR, MOVIE_DIRECTOR);
-		driver.quit();
+		Babye();
 	}
 }
