9f237e1f4970785f0180d75e1cab06f20b210079
sacrofer <38994980+sacrofer@users.noreply.github.com>
2018-05-14 23:11:30 -0500

    Adds support for finding a movie also considering the director

--- src/Imdb/Imdb_Methods.java
+++ src/Imdb/Imdb_Methods.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import Generics.Automation;
 
 public class Imdb_Methods extends Automation {
-	
+		
 	protected static void SearchMovie(String movie, String year) {
 		WebElement searchBox = driver.findElement(By.name("q"));
 		searchBox.clear();
@@ -20,24 +20,80 @@ public class Imdb_Methods extends Automation {
 		LocateCorrectMovie(movie, year);
 	}
 	
-	protected static WebElement LocateCorrectMovie(String movie, String year) {
-		List<WebElement> Containers = driver.findElements(By.className("result_text"));
+	protected static void LocateCorrectMovie(String movie, String year) {
+		By by = By.className("result_text");
+		WebElement correctElement = GetCorrectElement(by, movie, year);
 		
-		for (WebElement container : Containers) {
-			System.out.println(container.getText());
-			if (container.getText().contains(movie) && container.getText().contains(year)) {
-				container.findElement(By.linkText(movie)).click();
-				return container;
-			}
+		if (correctElement != null) {
+			correctElement.findElement(By.linkText(movie)).click();
+			System.out.println(movie + " from " + year + " found");
+		}else{
+			System.out.println("Test FAILED - " + movie + " from " + year + " NOT found");
+			Babye();
 		}
-		return null;
 	}
 	
-	protected static void VerifyMovie (String movie, String year) {
-		if (driver.getTitle().contains(movie) && driver.getTitle().contains(year)) {			
+	protected static void VerifyMovie (String movie, String year, String director) {
+		if (driver.getTitle().contains(movie) && driver.getTitle().contains(year)) {
+			VerifyDirector(director);
+		}else {
+			System.out.println("Looking for movie by name and year failed");
+			Babye();
+		}
+	}
+	
+	protected static void VerifyDirector(String director) {
+		if (IsDirectorInMainPage(director)) {
+			System.out.println("Test Passed");
+		}else if(IsDirectorInDetails(director)) {
 			System.out.println("Test Passed");
 		}else {
-			System.out.println("Test FAILED");
+			System.out.println("Test FAILED - Director not found");
+			Babye();
 		}
+	}
+
+	private static boolean IsDirectorInMainPage(String director) {
+		By xPath = By.xpath("//span[@itemprop='director']");
+		
+		WebElement correctElement = GetCorrectElement (xPath, director);		
+		
+		return (correctElement != null) ?  true :  false;
+	}
+
+	private static boolean IsDirectorInDetails(String director) {
+		System.out.println("Director is not in main page let's try in the details");
+		//TODO: Clarify error with this xPath "Invalid XPath"
+		String xPath = "//a[@href='fullcredits/?ref_=tt_ov_st_sm']";
+		WebElement DetailsLink = driver.findElement(By.xpath(xPath));
+		
+		//TODO: Clarify this kind of assert
+		//assert DetailsLink == null : String.format("Details link does not exist");
+		if (DetailsLink != null)
+			DetailsLink.click();
+		
+		if (driver.findElement(By.partialLinkText(director)).isDisplayed())
+			return true;
+		
+		return false;
+		
+	}
+	
+	private static WebElement GetCorrectElement (By by, String textToFind) {
+		List<WebElement> elements = driver.findElements(by);
+		for (WebElement element : elements) {
+			if (element.getText().contains(textToFind))
+				return element;
+		}
+		return null;		
+	}
+	
+	private static WebElement GetCorrectElement (By by, String textToFind, String textToFind2) {
+		List<WebElement> elements = driver.findElements(by);
+		for (WebElement element : elements) {
+			if (element.getText().contains(textToFind) && element.getText().contains(textToFind2))
+				return element;
+		}
+		return null;		
 	}
 }
